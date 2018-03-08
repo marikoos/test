261fd5ca5fa8edcb833d4dec2698d276e3b7e83f
Dale Hogg <dhdaleleon@gmail.com>
2018-03-09 10:54:20 +1300

    Please read the READ ME first

--- Staging/485 Design/js/scripts.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-    console.log( "Work" );
-});
-
-
-console.log("this is not the alphabet");
--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,32 @@ let gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	clean = require('gulp-clean'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	open = require('gulp-open');
 
 
 gulp.task("move", function(){
   console.log("TEMPLATE CREATED ON THE DESKTOP");
-  gulp.src('./Staging/485\ Design/**')
+  gulp.src('./Staging/485\ Design/dist/**')
     .pipe(gulp.dest('./Production/485\ Design\ (Production)'));
 });
 
 gulp.task('sass', function(){
-	gulp.src('./Staging/485\ Design/css/*.scss')
+	gulp.src('./Staging/485\ Design/src/css/*.scss')
 		.pipe(sass({outputStyle: "compressed"}))
-		.pipe(gulp.dest('./Staging/485\ Design/css'));
+		.pipe(gulp.dest('./Staging/485\ Design/dist/css'));
 });
 
-gulp.task('scripts', function(){
-	gulp.src('./Staging/485\ Design/js/*.js')
-		.pipe(concat('Full.js'))
+gulp.task('js', function(){
+	gulp.src('./Staging/485\ Design/src/js/*.js')
+		.pipe(concat('full.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('./Staging/485\ Design/js'));
-});
-
-gulp.task('clean', function(){
-	gulp.src(['./Production/485\ Design\ (Production)/**/**/*.scss','./Production/485\ Design\ (Production)/**/**/scripts.js','./Production/485\ Design\ (Production)/**/**/jquery.min.js'])
-		.pipe(clean({force: true}));
+		.pipe(gulp.dest('./Staging/485\ Design/dist/js'));
 });
 
 gulp.task('watch', function(){
-	gulp.watch('./Staging/485\ Design/css/*.scss', ['sass']);
+	gulp.watch('./Staging/485\ Design/src/css/*.scss', ['sass']);
+	gulp.watch('./Staging/485\ Design/src/js/*.js', ['js']);
+	gulp.src('./Staging/485\ Design/dist/index.html')
+		.pipe(open());
 });
-
-gulp.task('default', ['scripts']);
