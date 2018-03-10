3d91d73475efd624830b60583ea2f1db9b4db078
Dale Hogg <dhdaleleon@gmail.com>
2018-03-11 01:47:25 +1300

    Uncommented parts of gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 let gulp = require('gulp'),
-	// sass = require('gulp-sass'),
+	sass = require('gulp-sass'),
 	clean = require('gulp-clean'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
@@ -12,11 +12,11 @@ gulp.task("move", function(){
     .pipe(gulp.dest('./Production/485\ Design\ (Production)'));
 });
 
-// gulp.task('sass', function(){
-// 	gulp.src('./Staging/485\ Design/src/css/*.scss')
-// 		.pipe(sass({outputStyle: "compressed"}))
-// 		.pipe(gulp.dest('./Staging/485\ Design/dist/css'));
-// });
+gulp.task('sass', function(){
+	gulp.src('./Staging/485\ Design/src/css/*.scss')
+ 		.pipe(sass({outputStyle: "compressed"}))
+ 		.pipe(gulp.dest('./Staging/485\ Design/dist/css'));
+ });
 
 gulp.task('js', function(){
 	gulp.src('./Staging/485\ Design/src/js/*.js')
@@ -26,7 +26,7 @@ gulp.task('js', function(){
 });
 
 gulp.task('watch', function(){
-	// gulp.watch('./Staging/485\ Design/src/css/*.scss', ['sass']);
+	gulp.watch('./Staging/485\ Design/src/css/*.scss', ['sass']);
 	gulp.watch('./Staging/485\ Design/src/js/*.js', ['js']);
 	gulp.src('./Staging/485\ Design/dist/index.html')
 		.pipe(open());
